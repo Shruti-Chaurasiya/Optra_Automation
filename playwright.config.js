fc39b5ebee788bf64efbabfbe4d93e57c08dbd28
ShrutiChaurasiya <shruti.chaurasiya@indexnine.com>
2026-01-08 14:20:20 +0530

    headless:true

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     // baseURL: 'https://optra-uat.indexnine.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: !process.env.CI,
+    headless: true,
     // viewport: null,
     trace: 'on',
   },
